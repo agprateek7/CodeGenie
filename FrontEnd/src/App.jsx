c7538d9cc55e498bba9cfd0fca7bc8aced76407a
agprateek7 <agprateek7@gmai.com>
2025-05-09 19:25:24 +0530

    Updated backend API route

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {
+    const response = await axios.post("https://ai-code-reviewer-backend-b7qt.onrender.com/ai/get-review", {
       code: code
     })
     setCodereview(response.data)
