9a9ca1c1ab807e2220644e9776b7505e4a97eae8
agprateek7 <agprateek7@gmai.com>
2025-07-23 16:49:14 +0530

    Remove backend/.env and backend/node_modules from tracking and add to .gitignore

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -4,7 +4,15 @@ const cors = require('cors');
 // express -> node.js app framework that provide features like request, response etc.
 
 const app = express();
-app.use(cors())
+
+
+const corsOptions = {
+    origin: 'https://ai-code-reviewer-theta-taupe.vercel.app', // Use your "Domains" URL
+    methods: ['GET', 'POST'],
+    credentials: true,  // If you're using cookies or sessions
+  };
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
